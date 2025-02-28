68b0785ef2698821f3695a05c4d702716ee6a857
Szymon Mikulski <simmik@poczta.onet.pl>
2025-03-01 00:13:57 +0100

    Dodano funkcję czypalindrom

--- palindromy.py
+++ palindromy.py
@@ -1,11 +1,18 @@
-def czypalindrom(danytekst):
+def czypalindrom(dany_tekst):
     """
         Sprawdza, czy dany string jest palindromem, 
         tzn. czy czytany od przodu i od tyłu brzmi tak samo.
         Argument: string
     """
-    return False
+    tekst_bez_spacji = str(dany_tekst).replace(" ", "").lower()
+    i = 1
 
+    for i in range(0, len(tekst_bez_spacji)//2):
+        if tekst_bez_spacji[i] != tekst_bez_spacji[-(i+1)]:
+            return False
+    
+    return True
+   
 
 #debug line do usunięcia przed wysłaniem
-print(czypalindrom("kajak"))
+print(czypalindrom("Mamuta tu mam"))
