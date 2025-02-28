bed19a5814063dcb952eeda9401901b902a1c473
Szymon Mikulski <simmik@poczta.onet.pl>
2025-02-28 23:46:07 +0100

    Init commit

---
+++ palindromy.py
@@ -0,0 +1,11 @@
+def czypalindrom(danytekst):
+    """
+        Sprawdza, czy dany string jest palindromem, 
+        tzn. czy czytany od przodu i od tyłu brzmi tak samo.
+        Argument: string
+    """
+    return False
+
+
+#debug line do usunięcia przed wysłaniem
+print(czypalindrom("kajak"))
